71bc3474054d2395951ae1514d207484e0d87e5f
Dylan Awalt-Conley <web@dylan.ac>
2023-08-02 11:52:17 -0400

    install and configure tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import path from 'node:path'
+import sass from 'sass'
+import mkTailwindFunctions from 'sass-tailwind-functions'
+import react from '@astrojs/react'
 
-import react from "@astrojs/react";
+const tailwindFunctions = mkTailwindFunctions(
+  sass,
+  path.resolve('./tailwind.config.cjs'),
+)
 
 const tina = () => ({
   name: 'tina-cms',
@@ -10,13 +17,26 @@ const tina = () => ({
         name: 'tina',
         entrypoint: './client-directives/tina.mjs',
       })
-    }
-  }
+    },
+  },
 })
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://robertgdon.com',
   output: 'static',
-  integrations: [react(), tina()]
-});
+  integrations: [react(), tina()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          includePaths: ['node_modules', 'src/styles'],
+          quietDeps: true,
+          functions: {
+            ...tailwindFunctions,
+          },
+        },
+      },
+    },
+  },
+})
