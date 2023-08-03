8581111d57ed93b04ac9cc1db1faa6b31bd53569
Dylan Awalt-Conley <web@dylan.ac>
2023-08-03 15:08:10 -0400

    refactor project view components

---
+++ src/components/Loader.tsx
@@ -0,0 +1,5 @@
+export default () => (
+  <div className="flex flex-col items-center justify-center bg-neutral-800/75 text-white">
+    <i className="fa fa-spinner fa-pulse fa-3x" aria-hidden="true"></i>
+  </div>
+)
