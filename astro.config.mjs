6e772d839aeb2e20bad76e3177723e1582e951d2
Dylan Awalt-Conley <web@dylan.ac>
2023-09-21 22:53:38 -0400

    externalize tina client directive

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,26 +4,12 @@ import * as sass from 'sass'
 import react from '@astrojs/react'
 import mkTailwindFunctions from 'sass-tailwind-functions'
 import { sassLegacyFunctions } from './src/lib/node/image-config.ts'
+import tina from 'astro-tina'
 
 const tailwindFunctions = mkTailwindFunctions(
   sass,
   path.resolve('./tailwind.config.cjs'),
 )
-
-/**
- * @returns {import('astro').AstroIntegration}
- */
-const tina = () => ({
-  name: 'tina-cms',
-  hooks: {
-    'astro:config:setup': ({ addClientDirective }) => {
-      addClientDirective({
-        name: 'tina',
-        entrypoint: './client-directives/tina.mjs',
-      })
-    },
-  },
-})
 
 // https://astro.build/config
 export default defineConfig({
--- client-directives/client-directives.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'astro' {
-  interface AstroClientDirectives {
-    'client:tina'?: boolean
-  }
-}
-
-// Make d.ts a module to similate common packaging setups where the entry `index.d.ts` would augment the types
-export {}
