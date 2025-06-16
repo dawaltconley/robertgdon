8b2f3b78e4486785c3491d8cb4327ed491069475
Dylan Awalt-Conley <web@dylan.ac>
2025-06-16 01:24:16 -0400

    handle full embed links, if provided
    allows passing through e.g. a full vimeo embed link to the site

--- src/components/ProjectEmbed.tsx
+++ src/components/ProjectEmbed.tsx
@@ -21,10 +21,12 @@ const Container = ({ aspect, children }: ContainerProps) => (
   </div>
 )
 
+type EmbedType = 'bandcamp' | 'soundcloud' | 'youtube' | 'vimeo'
+
 interface ProjectEmbedProps extends ComponentPropsWithoutRef<'iframe'> {
   title: string
   link: string | URL
-  type: 'bandcamp' | 'soundcloud' | 'youtube' | 'vimeo'
+  type: EmbedType
   tralbumId?: string
 }
 
@@ -45,11 +47,7 @@ const ProjectEmbed: FunctionComponent<ProjectEmbedProps> = ({
         <Container aspect="square">
           <iframe
             className="w-full"
-            src={`https://bandcamp.com/EmbeddedPlayer/${url.pathname
-              .split('/')
-              .find(
-                Boolean,
-              )}=${tralbumId}/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/`}
+            src={getBandcampEmbedSrc(url, tralbumId)}
             {...props}
           >
             <a href={url.href}>{title}</a>
@@ -61,7 +59,7 @@ const ProjectEmbed: FunctionComponent<ProjectEmbedProps> = ({
         <Container aspect="square">
           <iframe
             className="w-full"
-            src={`https://w.soundcloud.com/player/?url=${url.href}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true`}
+            src={getSoundcloudEmbedSrc(url)}
             {...props}
           ></iframe>
         </Container>
@@ -71,7 +69,7 @@ const ProjectEmbed: FunctionComponent<ProjectEmbedProps> = ({
         <Container aspect="video">
           <iframe
             className="w-full"
-            src={url.href.replace('/watch?v=', '/embed/')}
+            src={getYoutubeEmbedSrc(url)}
             allowFullScreen
             {...props}
           ></iframe>
@@ -82,7 +80,7 @@ const ProjectEmbed: FunctionComponent<ProjectEmbedProps> = ({
         <Container aspect="video">
           <iframe
             className="w-full"
-            src={`https://player.vimeo.com/video${url.pathname}?title=0&byline=0&portrait=0`}
+            src={getVimeoEmbedSrc(url)}
             allowFullScreen
             {...props}
           ></iframe>
@@ -92,3 +90,57 @@ const ProjectEmbed: FunctionComponent<ProjectEmbedProps> = ({
 }
 
 export default ProjectEmbed
+
+function getBandcampEmbedSrc(link: string | URL, tralbumId: string): string {
+  const url = new URL(link)
+  if (url.pathname.startsWith('/EmbeddedPlayer/')) {
+    // if embed link, return unmodified
+    return url.href
+  } else {
+    // e.g. https://moretongues.bandcamp.com/track/keep-it-in
+    const type = url.pathname.split('/').find(Boolean) // e.g. track, album
+    return `https://bandcamp.com/EmbeddedPlayer/${type}=${tralbumId}/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/`
+  }
+}
+
+function getSoundcloudEmbedSrc(link: string | URL): string {
+  const url = new URL(link)
+  if (
+    url.hostname === 'w.soundcloud.com' &&
+    url.pathname.startsWith('/player/')
+  ) {
+    // if embed link, return unmodified
+    return url.href
+  } else {
+    // e.g. https://soundcloud.com/user-311803244/ink-the-octopus-humanly-possible?in=user-311803244/sets/ink-the-octopus
+    return `https://w.soundcloud.com/player/?url=${url.href}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true`
+  }
+}
+
+function getYoutubeEmbedSrc(link: string | URL): string {
+  const url = new URL(link)
+  return url.href.replace('/watch?v=', '/embed/')
+}
+
+function getVimeoEmbedSrc(link: string | URL): string {
+  const url = stripSearchParams(new URL(link))
+  if (url.hostname === 'player.vimeo.com') {
+    // if embed, return with modified search params
+    url.searchParams.set('title', '0')
+    url.searchParams.set('byline', '0')
+    url.searchParams.set('portrait', '0')
+    return url.href
+  } else {
+    // else, return embed url using video code
+    // e.g. https://vimeo.com/1075423559
+    return `https://player.vimeo.com/video${url.pathname}?title=0&byline=0&portrait=0`
+  }
+}
+
+function stripSearchParams(url: URL): URL {
+  const urlCopy = new URL(url)
+  for (const param of urlCopy.searchParams.keys()) {
+    urlCopy.searchParams.delete(param)
+  }
+  return urlCopy
+}
